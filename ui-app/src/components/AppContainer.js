c4075445e6968f383088316c4b71bcde0317a402
Noor Rizwan <noor.rizwan@microfocus.com>
2021-08-13 09:52:45 +0530

    Move code to API folder

---
+++ ui-app/src/components/AppContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './Header';
+
+
+const AppContainer = () => {
+    return <Header />
+};
+
+export default AppContainer;
---
+++ ui-app/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+    return <div className='header-panel h1'>This is header</div>
+};
+
+export default Header;
